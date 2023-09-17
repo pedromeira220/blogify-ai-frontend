3b4ecffe816a63aeba03afdab2b9216f69b2b0cb
Pedro Meira <pedrohmeira@icloud.com>
2023-09-17 13:00:46 -0300

    feat: initial commit (getting files from another project)

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './Container'
+import { Control, ControlProps } from './Control'
+import { Hint } from './Hint'
+import { Icon, IconProps } from './Icon'
+import { Label } from './Label'
+import { Root, RootProps } from './Root'
+
+export { Container, Control, Hint, Icon, Label, Root }
+export type { ControlProps, IconProps, RootProps }
